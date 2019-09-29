42ca128987d0d90628f482dc867c351762b2f448
RBarth01 <Rbarth01@baker.edu>
2019-09-29 09:43:23 -0400

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -118,13 +118,13 @@ class NewVisitorTest(StaticLiveServerTestCase):
         self.assertIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
-        
-        
+
+
     def test_layout_and_styling(self):
         # Edith goes to the home page
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
-        
+
         # She notices the input box is nicely centered
         inputbox = self.browser.find_element_by_id('id_new_item')
         self.assertAlmostEqual(
@@ -144,3 +144,4 @@ class NewVisitorTest(StaticLiveServerTestCase):
             512,
             delta=10
         )
+
